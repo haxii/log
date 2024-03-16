ac0c6ac4d9f3faf9f3bd3056556cbdea02e2795c
Zichao LI <cszichao@gmail.com>
2024-03-16 11:57:18 +0800

    better log backup

--- file.go
+++ file.go
@@ -1,66 +1,69 @@
 package log
 
 import (
+	"compress/gzip"
 	"fmt"
+	"github.com/pkg/errors"
+	"io"
 	"os"
 	"path/filepath"
 	"time"
 )
 
 func OpenLogFile(logDir, logName string) (*os.File, error) {
-	timeNOW := func() string {
-		return time.Now().Format("2006-01-02-15.04.05.999999999")
+	if err := createDirectories(logDir); err != nil {
+		return nil, err
 	}
 
-	logFileName := logName + ".log"
-	logFilePath := filepath.Join(logDir, logFileName)
-
-	if fileExists(logFilePath) {
-		_ = os.Rename(logFilePath, filepath.Join(logDir, logName+"."+timeNOW()+".log"))
-	}
-	logWriter, err := makefile(logDir, logFileName)
+	logFilePath, err := backupFile(logDir, logName)
+	fileFlag := os.O_CREATE | os.O_WRONLY
 	if err != nil {
-		return nil, err
+		fileFlag = fileFlag | os.O_APPEND // fail to back up, append it
+	} else {
+		fileFlag = fileFlag | os.O_TRUNC // backed up, truncate it
 	}
-	return logWriter, nil
-}
 
-// log file helper
-func makefile(dir string, filename string) (f *os.File, e error) {
-	if err := createDirectories(dir); err != nil {
-		return nil, err
-	}
-	filePath := filepath.Join(dir, filename)
-	fileWriter, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
-	if err != nil {
-		return nil, err
-	}
-	return fileWriter, nil
+	return os.OpenFile(logFilePath, fileFlag, 0644)
 }
 
 func createDirectories(dir string) error {
 	if fi, err := os.Stat(dir); err != nil {
 		if !os.IsNotExist(err) {
-			return fmt.Errorf("%v (checking directory)", err)
+			return errors.Wrap(err, "check directory failed")
 		}
-		if err := os.MkdirAll(dir, 0755); err != nil {
-			return fmt.Errorf("%v (creating directory)", err)
+		if err = os.MkdirAll(dir, 0755); err != nil {
+			return errors.Wrap(err, "create directory failed")
 		}
 	} else if !fi.IsDir() {
-		return fmt.Errorf("destination path is not directory")
+		return errors.New("destination path is not directory")
 	}
 	return nil
 }
 
-func fileExists(path string) bool {
-	if len(path) == 0 {
-		return false
-	}
+func backupFile(logDir, logName string) (logFilePath string, err error) {
+	logFileName := logName + ".log"
+	logFilePath = filepath.Join(logDir, logFileName)
 
-	if fi, err := os.Stat(path); os.IsNotExist(err) {
-		return false
-	} else if fi.IsDir() {
-		return false
+	f, fErr := os.Open(logFilePath)
+	if fErr != nil {
+		return //  we don't care this
 	}
-	return true
+	defer f.Close()
+
+	backupPath := filepath.Join(logDir, logName+"."+time.Now().Format("2006-01-02-15.04.05.999")+".log.gz")
+	g, gErr := os.Create(backupPath)
+	if gErr != nil {
+		fmt.Printf("fail to create log backup file %s with error %s\n", backupPath, gErr)
+		err = gErr
+		return
+	}
+	defer g.Close()
+
+	w := gzip.NewWriter(g)
+	defer w.Close()
+	if _, err = io.Copy(w, f); err != nil {
+		fmt.Printf("fail to backup file %s with error %s\n", backupPath, err)
+		return
+	}
+	return
 }
--- go.mod
+++ go.mod
@@ -1,8 +1,14 @@
 module github.com/haxii/log/v2
 
-go 1.16
+go 1.20
 
 require (
 	github.com/pkg/errors v0.9.1
-	github.com/rs/zerolog v1.23.0
+	github.com/rs/zerolog v1.32.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
